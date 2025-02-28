f98a6d3d68ca259cc4fa0e50e16443c864077581
Mr nadil <143246656+Nadilhansaja@users.noreply.github.com>
2025-02-28 20:37:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    /** ─── Auto Features ─────  */
+    autoLikeStatus: true,
+    /** ─── Owner Information ─── */
+    ownerNumber: '94773010580',
+
+    /** ─── Bot Details ──────── */
+    botDetails: {
+        botName: 'Fenix Id A7',
+        botLocation: 'Srilanka (🇱🇰)',
+        botAge: '26',
+        botEmail: 'privateloginemails@gmail.com',
+    },
+
+    /** ─── Fenix Custom Messages ────── */
+    fenixaboutype: 'Programming',  // Specify your value
+    fenixwel1: 'Your Number Has Been Auto Saved',
+    fenixwel2: 'Save And Alert Me If Save 🌹',
+    fenixwhatisay: 'ඔයගෙ නම්බරෙ ගත්තෙ ස්ටෙටස් එකකින් 🫠',
+
+};
