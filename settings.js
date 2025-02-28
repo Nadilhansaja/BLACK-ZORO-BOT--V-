ff00bacfa51f53fc3f21b747210aeef5a34d4a82
Mr nadil <143246656+Nadilhansaja@users.noreply.github.com>
2025-02-28 20:42:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=ezYwWI5A#N3cB1Iwg67oKVh5cfBoTAM-0EF3ghi_EFLuoROBN9S8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,94717202954' : process.env.SUDO,
